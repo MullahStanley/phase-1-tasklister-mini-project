f1208aa6296160e629e5ee414cf7b92d8b5dcfbf
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-03-25 08:43:13 +0300

    Assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form =document.querySelector('form')
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    createToDo(e.target.new-task-description.value)
+    form.reset()
+  })
 });
+
+function createToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDo)
+  btn.textContent= 'X'
+  p.textContent= `${todo}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteToDo(e){
+   e.target.parentNode.remove()
+}
